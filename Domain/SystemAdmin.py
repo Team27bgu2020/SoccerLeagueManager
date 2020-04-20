0d4057285a797089586e795746c720102442bbfd
IdanAlbilia <54328209+IdanAlbilia@users.noreply.github.com>
2020-04-20 19:27:34 +0300

    System Admin class

---
+++ Domain/SystemAdmin.py
@@ -0,0 +1,53 @@
+from Domain.SignedUser import SignedUser
+import Domain.Team as Team
+import Domain.TeamUser as TeamUser
+import Domain.RecommendationSystem as RecommendationSystem
+
+
+
+class SystemAdmin(SignedUser):
+    """ Constructor for SystemAdmin class getting arguments, checks them and updates the relevant fields"""
+
+    def __init__(self, user_name, password, name, birth_date):
+        super().__init__(user_name, password, name, birth_date)
+
+    """ This method closes a Team in the DB """
+
+    def close_team(self, team):
+        Team.type_check(team)
+
+        if team.is_open is False:
+            raise ValueError
+
+        team.close_team()
+
+    """ This method removes a user from the DB """
+
+    def remove_user(self, user):
+        SignedUser.type_check(user)
+        pass
+
+    """ This method shows complaints """
+
+    def show_complaints(self):
+        pass
+
+    """ This method allows to comment on a complaint """
+
+    def show_complaints(self, complaint):
+        pass
+
+    """ This method shows system log files """
+
+    def show_log_files(self):
+        pass
+
+    """ This method builds a recommendation system """
+
+    def build_recommendation_system(self):
+        pass
+
+
+def type_check(obj):
+    if type(obj) is not SystemAdmin:
+        raise TypeError
---
+++ UnitTests/test_systemAdmin.py
@@ -0,0 +1,44 @@
+import datetime as date
+from unittest import TestCase
+
+from Domain.SystemAdmin import SystemAdmin
+from Domain.SignedUser import SignedUser
+from Domain.PersonalPage import PersonalPage
+from Domain.Game import Game
+from Domain.Team import Team
+from Domain.RecommendationSystem import RecommendationSystem
+
+
+class TestSystemAdmin(TestCase):
+
+    teamA = Team("Arsenal")
+    teamB = Team("Manchester United")
+    user = SignedUser("userName", "password", "myName", date.datetime(2000, 1, 1))
+
+    def setUp(self):
+        user_name = 'default'
+        password = 'default'
+        name = 'default'
+        birth_date = date.datetime(2000, 1, 1)
+        self.SystemAdmin = SystemAdmin(user_name, password, name, birth_date)
+
+    def tearDown(self):
+        pass
+
+    """ Testing the follow personal page method """
+
+    def test_close_team(self):
+        self.assertRaises(TypeError, self.SystemAdmin.close_team, self.user)
+        self.SystemAdmin.close_team(self.teamA)
+        self.assertFalse(self.teamA.is_open, False)
+        self.SystemAdmin.close_team(self.teamB)
+        self.assertRaises(ValueError, self.SystemAdmin.close_team, self.teamB)
+
+
+
+
+
+
+
+
+
