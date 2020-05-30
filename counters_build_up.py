51c83d6209c1089fc1c05bcae0f5ecc520042ad9
Freimans <freimans@post.bgu.ac.il>
2020-05-30 18:32:02 +0300

    Counters build script added

---
+++ counters_build_up.py
@@ -0,0 +1,17 @@
+import pymongo
+
+HOST = "mongodb://localhost"
+PORT = 27017
+DB = "SoccerLeagueManagerDB"
+
+
+client = pymongo.MongoClient(HOST, PORT)
+db = client[DB]
+counters_collection = db["Counters"]
+
+counters_collection.insert_one({'game_counter': 1})
+counters_collection.insert_one({'complaints_counter': 1})
+counters_collection.insert_one({'game_event_counter': 1})
+counters_collection.insert_one({'league_counter': 1})
+counters_collection.insert_one({'page_counter': 1})
+counters_collection.insert_one({'user_counter': 1})
