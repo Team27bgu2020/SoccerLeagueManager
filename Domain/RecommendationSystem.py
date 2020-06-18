0f42669eb72c55e5dd03553018675ef1863fdcee
IdanAlbilia <54328209+IdanAlbilia@users.noreply.github.com>
2020-06-18 04:40:01 +0300

    Update RecommendationSystem.py

--- Domain/RecommendationSystem.py
+++ Domain/RecommendationSystem.py
@@ -31,6 +31,7 @@ football_data = sqlite3.connect('database.sqlite')
 player_df = pd.read_sql("SELECT * FROM Player;", football_data)
 player_stats_df = pd.read_sql("SELECT * FROM Player_Attributes;", football_data)
 team_df = pd.read_sql("SELECT * FROM Team;", football_data)
+team_stats_df = pd.read_sql("SELECT * FROM Team_Attributes;", football_data)
 match_df = pd.read_sql_query("SELECT *  From Match ", football_data)
 
 # Reduce match data to fulfill run time requirements
@@ -41,7 +42,7 @@ rows = ["country_id", "league_id", "season", "stage", "date", "match_api_id", "h
         "away_player_2", "away_player_3", "away_player_4", "away_player_5", "away_player_6",
         "away_player_7", "away_player_8", "away_player_9", "away_player_10", "away_player_11"]
 match_df.dropna(subset=rows, inplace=True)
-match_data = match_df
+match_data = match_df.tail(5000)
 
 
 # n_matches = match_df.shape[0]
@@ -55,7 +56,7 @@ match_data = match_df
 # print("Win rate of home team: {:.2f}%".format(win_rate))
 
 
-def get_last_matches(matches, date, team, x=10):
+def get_last_matches(matches, date, team, x=4):
     ''' Get the last x matches of a given team. '''
 
     # Filter team matches from matches
@@ -68,7 +69,7 @@ def get_last_matches(matches, date, team, x=10):
     return last_matches
 
 
-def get_last_matches_against_eachother(matches, date, home_team, away_team, x=10):
+def get_last_matches_against_eachother(matches, date, home_team, away_team, x=4):
     ''' Get the last x matches of two given teams. '''
 
     # Find matches of both teams
@@ -84,7 +85,7 @@ def get_last_matches_against_eachother(matches, date, home_team, away_team, x=10
                        0:total_matches.shape[0], :]
 
         # Check for error in data
-        if (last_matches.shape[0] > x):
+        if last_matches.shape[0] > x:
             print("Error in obtaining matches")
 
     # Return data
@@ -132,7 +133,80 @@ def get_wins(matches, team):
     return total_wins
 
 
-def get_match_features(match, matches, x=10):
+def get_build_up_play_speed(teams, team, date, x=1):
+    ''' Get the last x matches of a given team. '''
+
+    # Filter team from teams
+    team_stats = teams[(teams['team_api_id'] == team)]
+
+    # Filter x last forms from teams build up plays
+    last_play = team_stats[team_stats.date < date].sort_values(by='date', ascending=False).iloc[0:x, :]
+    try:
+        play_speed = int(last_play.buildUpPlaySpeed)
+    except:
+        return 40
+    # Return last matches
+    return play_speed
+
+
+def get_buildUp_stats(teams, team, date, x=1):
+    ''' Get the last x matches of a given team. '''
+
+    # Filter team from teams
+    team_stats = teams[(teams['team_api_id'] == team)]
+
+    # Filter x last forms from teams stats
+    last_play = team_stats[team_stats.date < date].sort_values(by='date', ascending=False).iloc[0:x, :]
+    try:
+        play_speed = int(last_play.buildUpPlaySpeed)
+        play_dribble = int(last_play.buildUpPlayDribbling)
+        play_pass = int(last_play.buildUpPlayPassing)
+    except:
+        return 120
+    # Return last matches
+    overall_build_up = play_speed + play_dribble + play_pass
+    return overall_build_up
+
+
+def get_chance_creation_stats(teams, team, date, x=1):
+    ''' Get the last x matches of a given team. '''
+
+    # Filter team from teams
+    team_stats = teams[(teams['team_api_id'] == team)]
+
+    # Filter x last forms from teams stats
+    last_play = team_stats[team_stats.date < date].sort_values(by='date', ascending=False).iloc[0:x, :]
+    try:
+        passing = int(last_play.chanceCreationPassing)
+        crossing = int(last_play.chanceCreationCrossing)
+        shooting = int(last_play.chanceCreationShooting)
+    except:
+        return 120
+    # Return last matches
+    overall_chance_creation = passing + crossing + shooting
+    return overall_chance_creation
+
+
+def get_defense_stats(teams, team, date, x=1):
+    ''' Get the last x matches of a given team. '''
+
+    # Filter team from teams
+    team_stats = teams[(teams['team_api_id'] == team)]
+
+    # Filter x last forms from teams stats
+    last_play = team_stats[team_stats.date < date].sort_values(by='date', ascending=False).iloc[0:x, :]
+    try:
+        pressure = int(last_play.defencePressure)
+        aggression = int(last_play.defenceAggression)
+        team_width = int(last_play.defenceTeamWidth)
+    except:
+        return 115
+    # Return last matches
+    overall_defense = pressure + aggression + team_width
+    return overall_defense
+
+
+def get_match_features(match, matches, teams, x=10):
     ''' Create match specific features for a given match. '''
 
     # Define variables
@@ -141,8 +215,8 @@ def get_match_features(match, matches, x=10):
     away_team = match.away_team_api_id
 
     # Get last x matches of home and away team
-    matches_home_team = get_last_matches(matches, date, home_team, x=10)
-    matches_away_team = get_last_matches(matches, date, away_team, x=10)
+    matches_home_team = get_last_matches(matches, date, home_team, x=4)
+    matches_away_team = get_last_matches(matches, date, away_team, x=4)
 
     # Get last x matches of both teams against each other
     last_matches_against = get_last_matches_against_eachother(matches, date, home_team, away_team, x=3)
@@ -153,12 +227,22 @@ def get_match_features(match, matches, x=10):
     home_goals_conceided = get_goals_conceided(matches_home_team, home_team)
     away_goals_conceided = get_goals_conceided(matches_away_team, away_team)
 
+    # Create team stats variables
+    home_buildUp_stats = get_buildUp_stats(teams, home_team, date)
+    away_buildUp_stats = get_buildUp_stats(teams, away_team, date)
+    home_chanceCreation_stats = get_chance_creation_stats(teams, home_team, date)
+    away_chanceCreation_stats = get_chance_creation_stats(teams, away_team, date)
+    home_defense_stats = get_defense_stats(teams, home_team, date)
+    away_defense_stats = get_defense_stats(teams, away_team, date)
+    home_overall_stats = home_buildUp_stats + home_chanceCreation_stats + home_defense_stats
+    away_overall_stats = away_buildUp_stats + away_chanceCreation_stats + away_defense_stats
+
     # Define result data frame
     result = pd.DataFrame()
 
     # Define ID features
     result.loc[0, 'match_api_id'] = match.match_api_id
-    result.loc[0, 'league_id'] = match.league_id
+    # result.loc[0, 'league_id'] = match.league_id
 
     # Create match features
     result.loc[0, 'home_team_goals_difference'] = home_goals - home_goals_conceided
@@ -167,6 +251,16 @@ def get_match_features(match, matches, x=10):
     result.loc[0, 'games_won_away_team'] = get_wins(matches_away_team, away_team)
     result.loc[0, 'games_against_won'] = get_wins(last_matches_against, home_team)
     result.loc[0, 'games_against_lost'] = get_wins(last_matches_against, away_team)
+    result.loc[0, 'home_build_up_play'] = get_build_up_play_speed(teams, home_team, date)
+    result.loc[0, 'away_build_up_play'] = get_build_up_play_speed(teams, away_team, date)
+    result.loc[0, 'home_buildUp_stats'] = home_buildUp_stats
+    result.loc[0, 'away_buildUp_stats'] = away_buildUp_stats
+    result.loc[0, 'home_chanceCreation_stats'] = home_chanceCreation_stats
+    result.loc[0, 'away_chanceCreation_stats'] = away_chanceCreation_stats
+    result.loc[0, 'home_defense_stats'] = home_defense_stats
+    result.loc[0, 'away_defense_stats'] = away_defense_stats
+    result.loc[0, 'home_overall_stats'] = home_overall_stats
+    result.loc[0, 'away_overall_stats'] = away_overall_stats
 
     # Return match features
     return result.loc[0]
@@ -196,7 +290,7 @@ def get_match_label(match):
     return label.loc[0]
 
 
-def create_feables(matches, x=10, verbose=True):
+def create_features(matches, teams, x=10, verbose=True):
     ''' Create and aggregate features and labels for all matches. '''
 
     if verbose is True:
@@ -204,7 +298,7 @@ def create_feables(matches, x=10, verbose=True):
     start = time()
 
     # Get match features for all matches
-    match_stats = matches.apply(lambda x: get_match_features(x, matches, x=10), axis=1)
+    match_stats = matches.apply(lambda x: get_match_features(x, matches, teams, x=10), axis=1)
 
     # Create dummies for league ID feature
     # dummies = pd.get_dummies(match_stats['league_id']).rename(columns=lambda x: 'League_' + str(x))
@@ -221,38 +315,38 @@ def create_feables(matches, x=10, verbose=True):
 
     # Create match labels
     labels = match_df.apply(get_match_label, axis=1)
-    # match_df.insert(6, "FTR", FTR, True)
-    # match_df['FTR'] = FTR
     end = time()
     if verbose is True:
         print("Match labels generated in {:.1f} minutes".format((end - start) / 60))
 
     # Merges features and labels into one frame
-    feables = pd.merge(match_stats, labels, on='match_api_id', how='left')
+    features = pd.merge(match_stats, labels, on='match_api_id', how='left')
 
     # Drop NA values
-    feables.dropna(inplace=True)
+    features.dropna(inplace=True)
 
     # Return preprocessed data
-    return feables
+    return features
 
 
-feables = create_feables(match_data)
-cols = feables.columns.tolist()
-display(feables.head())
+features = create_features(match_data, team_stats_df)
+cols = features.columns.tolist()
+display(features.head())
 display(cols)
 
 # Separate into feature set and target variable
 # FTR = Full Time Result (H=Home Win, D=Draw, A=Away Win)
-X_all = feables.drop(['label'], 1)
+X_all = features.drop(['label'], 1)
 # display(X_all.columns.tolist())
-y_all = feables['label']
+y_all = features['label']
 
 # Standardising the data.
 
 # Center to the mean and component wise scale to unit variance.
 cols = [['home_team_goals_difference', 'away_team_goals_difference', 'games_won_home_team', 'games_won_away_team',
-         'games_against_won', 'games_against_lost']]
+         'games_against_won', 'games_against_lost', 'home_buildUp_stats', 'away_buildUp_stats',
+         'home_chanceCreation_stats', 'away_chanceCreation_stats', 'home_defense_stats', 'away_defense_stats',
+         'home_build_up_play', 'away_build_up_play', 'home_overall_stats', 'away_overall_stats']]
 for col in cols:
     X_all[col] = scale(X_all[col])
 
@@ -261,7 +355,7 @@ display(X_all.head())
 
 # Shuffle and split the dataset into training and testing set.
 X_train, X_test, y_train, y_test = train_test_split(X_all, y_all,
-                                                    test_size=50,
+                                                    test_size=100,
                                                     random_state=2,
                                                     stratify=y_all)
 
